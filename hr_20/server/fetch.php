e7f85d8d872bb78de368bd6f6eea23072dd9b21d
pavanpw <pavan.kalyan@salussg.com>
2020-11-18 10:57:46 +0530

    update code changes

--- hr_20/server/fetch.php
+++ hr_20/server/fetch.php
@@ -23,14 +23,13 @@
                 echo "<tr><td>".$row['name']."</td>";
                 echo "<td>".$row['mobile']."</td>";
                 echo "<td>".$row['email']."</td>";
-                echo "<td> <a href='./delete.php?id=".$row['id']."'><button class='btn btn-danger'>Delete</button></td></tr></a>";
+                echo "<td> <a href='./delete.php?id=".$row['id']."'><button class='btn btn-danger'>Delete</button></a> <a href='./edit.php?id=".$row['id']."'> <button class='btn btn-success'>Update</button></a></td></tr>";
             }
             echo "</table>";
             
         }else{
             echo "No Data found";
         }
-
     ?>
 </body>
 </html>
